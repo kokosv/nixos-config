167934bbeb73d0a07cc08b7464b0e888bb3c57a5
kokosv <kaloyansv@gmail.com>
2026-06-10 18:49:24 +0300

    updated tuigreet

--- modules/system/display-manager.nix
+++ modules/system/display-manager.nix
@@ -14,9 +14,11 @@ in
 
     services.greetd = {
       enable = true;
+      useTextGreeter = true;
+
       settings = {
         default_session = {
-          command = ''${pkgs.tuigreet}/bin/tuigreet --time --time-format "[w'%W]   [d'%j]   [%a]   [%Y/%m/%d]   [%H:%M:%S]   [%z]" --window-padding 2 --greeting '"An idiot admires complexity, a genius admires simplicity" - Terry Davis' --asterisks --remember --remember-user-session --xsessions "${config.services.displayManager.sessionData.desktops}/share/xsessions" --xsession-wrapper "startx /usr/bin/env" -d '';
+          command = ''${pkgs.tuigreet}/bin/tuigreet --time --time-format "[w%W]   [d%j]   [%a]   [%Y/%m/%d]   [%H:%M:%S]   [%z]" --window-padding 2 --greeting "An idiot admires complexity, a genius admires simplicity - Terry Davis" --asterisks --remember --remember-user-session --xsessions ${config.services.displayManager.sessionData.desktops}/share/xsessions --xsession-wrapper "startx /usr/bin/env" -d '';
         };
       };
     };
